d25621d41ba5be15a742364a7ed387297f94c0e6
vitorsandrade <vitorsantos.dandrade@gmail.com>
2023-03-08 16:47:49 -0300

    service and resource impl

---
+++ src/main/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.dicasdeumdev.apitestes.services.impl;
+
+import br.com.dicasdeumdev.apitestes.domain.User;
+import br.com.dicasdeumdev.apitestes.repositories.UserRepository;
+import br.com.dicasdeumdev.apitestes.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
