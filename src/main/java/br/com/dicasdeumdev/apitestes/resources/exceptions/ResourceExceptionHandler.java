7e3d66d2a3fc985a041e566c0ecc0ce1116e5525
vitorsandrade <vitorsantos.dandrade@gmail.com>
2023-03-09 10:50:13 -0300

    execption e-mail

--- src/main/java/br/com/dicasdeumdev/apitestes/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/dicasdeumdev/apitestes/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.apitestes.resources.exceptions;
 
+import br.com.dicasdeumdev.apitestes.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.apitestes.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -22,5 +23,17 @@ public class ResourceExceptionHandler {
                 request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError> dataIntegratyViolation(DataIntegratyViolationException ex,
+                                                        HttpServletRequest request) {
+
+        StandardError error = new StandardError(LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/apitestes/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.dicasdeumdev.apitestes.domain.User;
 import br.com.dicasdeumdev.apitestes.domain.dto.UserDTO;
 import br.com.dicasdeumdev.apitestes.repositories.UserRepository;
 import br.com.dicasdeumdev.apitestes.services.UserService;
+import br.com.dicasdeumdev.apitestes.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.apitestes.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return repository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO dto){
+        Optional<User> user = repository.findByEmail(dto.getEmail());
+        if(user.isPresent()) {
+            throw new DataIntegratyViolationException("E-mail já cadastrado no sistema!");
+        }
     }
 }
